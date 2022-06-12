b7c299e90a0e76f3e76fccead681d09297b65436
西海大愛 <71867595+nishiumidaina@users.noreply.github.com>
2022-06-13 06:22:58 +0900

    first commit
    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Spot;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = \Auth::user();
+        $spots = Spot::get();
+        return view('home', compact('user', 'spots'));
+    }
+        public function create()
+    {
+        $user = \Auth::user();
+        $spots = Spot::get();
+        return view('create', compact('user','spots'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $spot_id = Spot::insertGetId([
+            'name' => $data['name'],
+             'longitude' => $data['longitude'], 
+             'latitude' => $data['latitude'],
+             'url' => $data['url'],
+             'ex' => $data['ex'],
+             'status' => 'None'
+        ]);
+        // リダイレクト処理
+        return redirect()->route('home');
+    }
+    public function edit($id){
+        // IDをデータベースから取得
+        $user = \Auth::user();
+        $spot = Spot::where('id', $id)->first();
+        $spots = Spot::get();
+        //   dd($memo);
+        //取得したメモをViewに渡す
+        return view('edit',compact('user','spot','spots'));
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $inputs = $request->all();
+        // dd($inputs);
+         Spot::where('id', $id)->delete();
+        return redirect()->route('home')->with('success', '削除が完了しました！');
+    }
+
+    public function start(Request $request, $id)
+    {
+        $inputs = $request->all();
+        //判定
+        Spot::where('id', $id)->update(['status'=>'Start']);
+
+        //$command = 'python Python/yolov5/detect.py --source "https://www.youtube.com/watch?v=DjdUEyjx8GM"> /dev/null &';
+        //直接detect.pyを送ると終了できないため一時的にこちらを使う
+        $command = 'python Python/yolov5_DeepSort_Pytorch/start.py> /dev/null &';
+        exec($command) ;
+        return redirect()->route('home')->with('success', '実行が完了しました！');
+    }
+    public function stop(Request $request, $id)
+    {
+        $inputs = $request->all();
+        //判定
+        Spot::where('id', $id)->update(['status'=>'Stop']);
+        return redirect()->route('home')->with('success', '停止が完了しました！');
+    }
+
+}
---
+++ database/migrations/2022_06_11_170907_create_spots_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spots', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->double('latitude', 10, 7);
+            $table->double('longitude', 10, 8);
+            $table->text('ex');
+            $table->text('url');
+            $table->text('status')->defalut('None');
+            // timestampと書いてしまうと、レコード挿入時、更新時に値が入らないので、DB::rawで直接書いてます
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spots');
+    }
+}
